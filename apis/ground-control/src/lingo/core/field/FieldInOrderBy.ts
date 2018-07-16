146408bb24a88b4affc5b1290b0a670e9d27db74
russoturisto <russoturisto@gmail.com>
2018-07-16 09:16:40 -0600

    trying out typescript 3.0 project references

--- apis/ground-control/src/lingo/core/field/FieldInOrderBy.ts
+++ apis/ground-control/src/lingo/core/field/FieldInOrderBy.ts
@@ -1,28 +1,38 @@
+
 /**
  * Order of a sorted field, as specified in the ORDER BY clause.
  */
-export declare enum SortOrder {
-    ASCENDING = 0,
-    DESCENDING = 1
+export enum SortOrder {
+	ASCENDING,
+	DESCENDING
 }
+
 /**
  * A serialized version of a query field as specified in the GROUP BY clause.
  */
 export interface JSONFieldInGroupBy {
-    fa: string;
+	// Field Alias
+	fa: string;
 }
+
 /**
  * A serialized version of a query field as specified in the ORDER BY clause.
  */
 export interface JSONFieldInOrderBy extends JSONFieldInGroupBy {
-    so: SortOrder;
+	// Sort Order
+	so: SortOrder;
 }
+
 /**
  * A serialized version of an entity field (column) as specified in the ORDER BY clause.
  */
 export interface JSONEntityFieldInOrderBy extends JSONFieldInOrderBy {
-    si: number;
-    ti: number;
-    pi: number;
-    ci: number;
+	// Schema Version Id
+	si: number,
+	// Entity Type Index
+	ti: number,
+	// Property Index
+	pi: number,
+	// Column Index
+	ci: number
 }
