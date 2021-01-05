1aa8485880a4e9d6835c1db668189ce0dda100d3
papa <russoturisto@gmail.com>
2021-01-05 06:22:07 -0700

    Formatting settings change.

--- generators/runway/src/index.ts
+++ generators/runway/src/index.ts
@@ -2,58 +2,57 @@
  * Created by Papa on 4/24/2016.
  */
 
-import * as fs             from 'fs'
-import {readConfiguration} from './ddl/options/generator'
-import {watchFiles}        from './FileWatcher'
+import * as fs               from 'fs';
+import { readConfiguration } from './ddl/options/generator';
+import { watchFiles }        from './FileWatcher';
 
-const configuration = readConfiguration(process.cwd(), process.argv)
-globalThis.configuration = configuration
+const configuration      = readConfiguration(process.cwd(), process.argv);
+globalThis.configuration = configuration;
 
-const ddlDirPath    = process.cwd() + '/' + configuration.airport.ddlDir
-let sourceFilePaths = findAllSourceFilePaths(ddlDirPath)
+const ddlDirPath    = process.cwd() + '/' + configuration.airport.ddlDir;
+let sourceFilePaths = findAllSourceFilePaths(ddlDirPath);
 
 if (configuration.airport.daoDir) {
-	const daoDirPath         = process.cwd() + '/' + configuration.airport.daoDir
-	const daoSourceFilePaths = findAllSourceFilePaths(daoDirPath)
-	sourceFilePaths          = [...daoSourceFilePaths, ...sourceFilePaths]
+	const daoDirPath         = process.cwd() + '/' + configuration.airport.daoDir;
+	const daoSourceFilePaths = findAllSourceFilePaths(daoDirPath);
+	sourceFilePaths          = [...daoSourceFilePaths, ...sourceFilePaths];
 }
-
 
 function findAllSourceFilePaths(
 	dirPath: string,
 ): string[] {
-	if (!fs.existsSync(dirPath)){
+	if (!fs.existsSync(dirPath)) {
 		fs.mkdirSync(dirPath);
 	}
-	const allFileNames       = fs.readdirSync(dirPath)
+	const allFileNames       = fs.readdirSync(dirPath);
 	const containedFilePaths = allFileNames.map(
 		fileName => {
-			return dirPath + '/' + fileName
-		})
-	let sourceFilePaths      = []
+			return dirPath + '/' + fileName;
+		});
+	let sourceFilePaths      = [];
 	const subDirectoryPaths  = containedFilePaths.filter(
 		filePath => {
 			if (fs.lstatSync(filePath).isDirectory()) {
-				return true
+				return true;
 			} else {
 				if (isTsFile(filePath)) {
-					sourceFilePaths.push(filePath)
+					sourceFilePaths.push(filePath);
 				}
-				return false
+				return false;
 			}
 		}
-	)
+	);
 	for (const subDirPath of subDirectoryPaths) {
-		sourceFilePaths = sourceFilePaths.concat(findAllSourceFilePaths(subDirPath))
+		sourceFilePaths = sourceFilePaths.concat(findAllSourceFilePaths(subDirPath));
 	}
 
-	return sourceFilePaths
+	return sourceFilePaths;
 }
 
 function isTsFile(
 	fileName: string
 ): boolean {
-	return fileName.substr(fileName.length - 3, 3) === '.ts'
+	return fileName.substr(fileName.length - 3, 3) === '.ts';
 }
 
 // Start the watcher
