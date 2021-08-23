2ff89ee6b031eabff2858bb5f77128e327637717
artem-v-shamsutdinov <artem-v-shamsutdinov@dataindependence.net>
2021-08-23 13:04:21 -0600

    Note to see if loading of AirportDatabase is needed by runway

--- generators/runway/src/FileWatcher.ts
+++ generators/runway/src/FileWatcher.ts
@@ -24,6 +24,7 @@ import { Configuration }               from './ddl/options/Options';
 import { EntityCandidate }             from './ddl/parser/EntityCandidate';
 import { generateDefinitions }         from './FileProcessor';
 
+// TODO: figure out if this is needed
 (AirportDatabase as any).bogus = 'loaded for schema generation';
 
 /**
