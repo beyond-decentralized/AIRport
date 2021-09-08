e0f410b7793d6a5c1ea712d07774b12f7b257821
russoturisto <russoturisto@gmail.com>
2021-09-08 12:48:09 -0600

    Got svelte to work in functionality-demo

--- apps/functionality-demo/src/App.svelte
+++ apps/functionality-demo/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	let name: string = 'world';
+	export let name: string;
 </script>
 
 <main>
@@ -7,21 +7,23 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style lang="scss">
+<style>
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
 
-		h1 {
-			color: #ff3e00;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
 
-		@media (min-width: 640px) {
+	@media (min-width: 640px) {
+		main {
 			max-width: none;
 		}
 	}
