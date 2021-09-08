1d57d5d8dbf558161ac024c61202f36186e87eab
russoturisto <russoturisto@gmail.com>
2021-09-07 19:34:47 -0600

    Change functionality-demo to typescript version.

--- apps/functionality-demo/src/App.svelte
+++ apps/functionality-demo/src/App.svelte
@@ -1,5 +1,5 @@
-<script>
-	export let name;
+<script lang="ts">
+	let name: string = 'world';
 </script>
 
 <main>
@@ -7,23 +7,21 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+		h1 {
+			color: #ff3e00;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
 
-	@media (min-width: 640px) {
-		main {
+		@media (min-width: 640px) {
 			max-width: none;
 		}
 	}
