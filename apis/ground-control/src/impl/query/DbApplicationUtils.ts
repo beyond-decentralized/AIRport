25f12cc751759764ab2a572e4bb6f26d3b10aba7
russoturisto <russoturisto@gmail.com>
2022-01-08 16:43:06 -0700

    Added colon (':') to domain name processing.

--- apis/ground-control/src/impl/query/DbApplicationUtils.ts
+++ apis/ground-control/src/impl/query/DbApplicationUtils.ts
@@ -21,16 +21,18 @@ export function getFullApplicationNameFromDomainAndName(
 
 	if (domainName.endsWith('.')
 		|| domainName.endsWith('-')
+		|| domainName.endsWith(':')
 		|| domainName.endsWith('__')) {
-		throw new Error('Domain Name cannot end with ".", "-" or "__"');
+		throw new Error('Domain Name cannot end with ".", "-", ":" or "__"');
 	}
 
 	const domainPrefix = domainName
 		.replace(/\./g, '_dot_')
-		.replace(/-/g, '_dash_');
+		.replace(/-/g, '_dash_')
+		.replace(/:/g, '_colon_');
 
 	if (domainPrefix.indexOf('___') > -1) {
-		throw new Error('Domain Name cannot have with ".", "-" or "_" right next to each other.');
+		throw new Error('Domain Name cannot have with ".", "-", ":", or "_" right next to each other.');
 	}
 
 	if (applicationName.indexOf('_') > -1) {
