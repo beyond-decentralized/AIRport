c0dcebc1511a03bf144e1469b1d583c6c7a80f58
russoturisto <russoturisto@gmail.com>
2022-01-10 10:48:41 -0700

    Continuing to work on AIRport in a separate tab.

--- execs/app-frame/src/frame.ts
+++
@@ -1,5 +0,0 @@
-import { markerFunction } from '@airport/functionality-demo-schema/lib/server'
-
-markerFunction()
-
-export const marker = 1
--- execs/web-airport/src/App.svelte
+++ execs/web-airport/src/App.svelte
@@ -1,19 +1,4 @@
 <style type="text/css">
-body,
-html {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  overflow: hidden;
-}
-
-#content {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0px;
-}
 </style>
 
 <script lang="ts">
@@ -26,8 +11,6 @@ page('/:uiPath', (context) => {
 page()
 
 onMount(async () => {
-  (document.getElementById('ui') as HTMLIFrameElement).src =
-    'http://' + window.location.hash.substring(2);
 });
 </script>
 
@@ -39,12 +22,3 @@ onMount(async () => {
   style="display: none;"
   title="votecube"></iframe>
 
-<div id="content">
-  <iframe
-    id="ui"
-    class="ui"
-    name="votecube-ui"
-    src=""
-    style="width: 100%; height: 100%;"
-    title="votecube-ui"></iframe>
-</div>
