698d616034b21625f8a79ef94a2321f1c98e2395
russoturisto <russoturisto@gmail.com>
2022-01-27 10:39:03 -0700

    Formatting.

--- apis/cross-terminal-transfer/src/tokens.ts
+++ apis/cross-terminal-transfer/src/tokens.ts
@@ -1,6 +1,6 @@
-import {lib}                     from '@airport/di'
-import {ISyncConnectionServer}      from './lingo/SyncConnectionServer'
+import { lib } from '@airport/di'
+import { ISyncConnectionServer } from './lingo/SyncConnectionServer'
 
 const arrivalsNDepartures = lib('arrivals-n-departures')
 
-export const SYNC_CONNECTION_SERVER       = arrivalsNDepartures.token<ISyncConnectionServer<any, any, any, any>>('ISyncConnectionServer')
+export const SYNC_CONNECTION_SERVER = arrivalsNDepartures.token<ISyncConnectionServer<any, any, any, any>>('ISyncConnectionServer')
