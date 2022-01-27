981f557a2fea67a8ffa64ede433c00f8d44bb57c
russoturisto <russoturisto@gmail.com>
2022-01-27 10:31:44 -0700

    Updated dependency injection to common lib call.

--- apis/cross-terminal-transfer/src/tokens.ts
+++ apis/cross-terminal-transfer/src/tokens.ts
@@ -1,6 +1,6 @@
-import {system}                     from '@airport/di'
+import {lib}                     from '@airport/di'
 import {ISyncConnectionServer}      from './lingo/SyncConnectionServer'
 
-const arrivalsNDepartures = system('airport').lib('arrivals-n-departures')
+const arrivalsNDepartures = lib('arrivals-n-departures')
 
 export const SYNC_CONNECTION_SERVER       = arrivalsNDepartures.token<ISyncConnectionServer<any, any, any, any>>('ISyncConnectionServer')
