03532c6d6c5c003aaa7d30d6c3142335805fb7ba
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2022-07-24 16:29:45 -0600

    Removed Function definition in favor of inlined (due to compilation problems).

---
+++ libs/airbridge/validate/src/implementation/Functions.ts
@@ -0,0 +1,35 @@
+import { IValidationEntity } from "../definition/IValidationEntity";
+import { IValidationField } from "../definition/IValidationField";
+import { IValidationNumber } from "../definition/IValidationNumber";
+
+export const between = function (
+    from: number,
+    to: number
+): IValidationNumber {
+    return null
+}
+
+export const equals = function <T, F extends IValidationField<T>>(
+    value: T | F
+): F {
+    return null
+}
+
+export const exists = function <T, E extends IValidationEntity<T>>(
+    validationSpec?: T
+): E {
+    return null
+}
+
+export const isNull = function <T, E extends IValidationEntity<T>>(
+    validationSpec?: T
+): E {
+    return null
+}
+
+export const or = function <T, F extends IValidationField<T>>(
+    ...conditions: (T | F)[]
+): F {
+    return null
+}
+
