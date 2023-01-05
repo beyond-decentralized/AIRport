b75a3e6dae98fcd6a9ef21adbe6638169e288913
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2023-01-05 08:10:18 -0700

    Added @airbridge/keyring to schema blueprint.

--- libs/blueprint/src/index.ts
+++ libs/blueprint/src/index.ts
@@ -4,6 +4,7 @@ import { APPLICATION as AIRSPACE } from '@airport/airspace/dist/definition/appli
 import { APPLICATION as TRAVEL_DOCUMENT_CHECKPOINT } from '@airport/travel-document-checkpoint/dist/definition/application'
 import { APPLICATION as LAYOVER } from '@airport/layover/dist/definition/application'
 import { APPLICATION as SESSION_STATE } from '@airport/session-state/dist/definition/application'
+import { APPLICATION as KEYRING } from '@airbridge/keyring/dist/definition/application'
 
 export const BLUEPRINT = [
 	AIRPORT_CODE,
@@ -11,5 +12,6 @@ export const BLUEPRINT = [
 	TRAVEL_DOCUMENT_CHECKPOINT,
 	HOLDING_PATTERN,
 	LAYOVER,
-	SESSION_STATE
+	SESSION_STATE,
+	KEYRING
 ]
