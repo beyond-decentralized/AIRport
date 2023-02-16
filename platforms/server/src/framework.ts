17de6d9c1c27ffc08ae3e6e0d300c3b13b2048f8
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2023-02-16 08:47:23 -0700

    #36 bug fix

--- platforms/server/src/framework.ts
+++ platforms/server/src/framework.ts
@@ -2,6 +2,7 @@ import { loadAirTrafficControl } from '@airport/air-traffic-control'
 import { startDb } from '@airport/sqljs'
 import { injectTransactionalConnector, injectTransactionalServer } from '@airport/terminal'
 import { injectAirportDatabase, injectEntityStateManager } from '@airport/tower'
+import { loadSessionStateApi } from '@airport/session-state/dist/app/bundle'
 
 console.log('Injecting Server Source')
 loadAirTrafficControl()
@@ -9,6 +10,7 @@ injectTransactionalConnector()
 injectAirportDatabase()
 injectTransactionalServer()
 injectEntityStateManager()
+loadSessionStateApi()
 
 export async function initServer() {
     await startDb('AIRport-prerelease')
